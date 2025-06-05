2ca2f8bfad1ec9f1cfaf954c95ce5cd8b8f31068
ShivaNaniSimha <shivananisimha@gmail.com>
2025-06-05 13:35:47 +0530

    updated changes

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "dev-nani-tf-bucket"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
